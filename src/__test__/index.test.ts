17229247875c2c07bb4c75714f12561b98478299
dqn <dqn270@gmail.com>
2021-02-16 00:50:03 +0900

    feat: implement post extension

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,18 +1,25 @@
-import { getPrograminfo, getExtension } from "./..";
+import { getPrograminfo, getExtension, postExtension } from "./..";
 
 const userSession = process.env.USER_SESSION!;
 const nicoliveProgramId = process.env.NICOLIVE_PROGRAM_ID!;
 
-describe("getPrograminfo", () => {
-  xit("test", async () => {
+describe("programinfo", () => {
+  xit("post", async () => {
     const res = await getPrograminfo(userSession, nicoliveProgramId);
     console.log(JSON.stringify(res));
   });
 });
 
-describe("getExtension", () => {
-  it("test", async () => {
+describe("extension", () => {
+  xit("get", async () => {
     const res = await getExtension(userSession, nicoliveProgramId);
+    console.log(JSON.stringify(res));
+  });
+
+  it("post", async () => {
+    const res = await postExtension(userSession, nicoliveProgramId, {
+      minutes: 30,
+    });
     console.log(JSON.stringify(res));
   });
 });
--- src/extension.ts
+++ src/extension.ts
@@ -24,3 +24,30 @@ export function getExtension(
     },
   });
 }
+
+export type PostExtensionRequest = {
+  minutes: 30 | 60 | 90 | 120 | 150 | 180 | 210 | 240 | 270 | 300 | 330;
+};
+
+export type PostExtensionData = {
+  end_time: number;
+};
+
+export type PostExtensionResponse = NicoliveApiResponse<PostExtensionData>;
+
+export function postExtension(
+  userSession: string,
+  nicoliveProgramId: string,
+  body: PostExtensionRequest,
+): Promise<PostExtensionResponse> {
+  return request(`/watch/${nicoliveProgramId}/extension`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      cookie: serializeCookie({
+        user_session: userSession,
+      }),
+    },
+    body: JSON.stringify(body),
+  });
+}
